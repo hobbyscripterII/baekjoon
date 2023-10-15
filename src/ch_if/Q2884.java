45175e660c1e7a1e487d02bb3b2b5ba3fcf487d3
hobbyscripterII <wruomma2@naver.com>
2023-10-15 15:51:03 +0900

    2884

--- src/ch_if/Q2884.java
+++ src/ch_if/Q2884.java
@@ -4,18 +4,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Q2884 {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] arr = br.readLine().split(" ");
-		int h = Integer.parseInt(arr[0]), m = Integer.parseInt(arr[1]);
-		
-		if(h == 0 && m >= 45) {
-			m -= 45;
-		} else {
-			h = h == 0 ? 23 : (h -= 1);
-			m = m - 45 < 0 ? m += 60 - 45 : (m -= 45);
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int h = Integer.parseInt(arr[0]), m = Integer.parseInt(arr[1]);
 
-		System.out.println(h + " " + m);
-	}
+        // 1: if문
+        if(m >= 45) {
+            m -= 45;
+        } else {
+            h = h != 0 ? --h : 23;
+            m += 60 - 45;
+        }
+
+        // 2: 삼항 연산자
+        h = m >= 45 ? h : (h == 0 ? 23 : --h);
+        m = m >= 45 ? (m - 45) : (m + 60 - 45);
+
+        System.out.println(h + " " + m);
+    }
 }
