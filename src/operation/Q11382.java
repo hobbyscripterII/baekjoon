cbe92d09e0716743c8963493e2d36de904c10f04
hobbyscripterII <wruomma2@naver.com>
2023-10-15 23:08:07 +0900

    11382

---
+++ src/operation/Q11382.java
@@ -0,0 +1,18 @@
+package operation;
+
+import java.util.Scanner;
+
+public class Q11382 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long[] val = new long[3];
+        long result = 0;
+
+        for (int i = 0; i < val.length; i++) {
+            val[i] = s.nextLong();
+            result += val[i];
+        }
+
+        System.out.println(result);
+    }
+}
